30eef64a625c1519865c9dd77e2461b1d7377078
YurGoV <yurgov@gmail.com>
2023-07-19 12:36:22 +0300

    ADD: swagger doc completed, removed unused libraries, code cleanup

--- controllers/parser.js
+++ controllers/parser.js
@@ -3,27 +3,23 @@ const { getWordsArray, parseWordsArray, parseString } = require("../utils");
 const parserCtrl = (req, res) => {
   const { text } = req.body
   const message = { message: 'there are no unique letter in text' }
-  // const letter = uniqueSymbolFinder(text)
   const wordsArray = getWordsArray(text)
-  // console.log(wordsArray)
   if (wordsArray.length === 0) {
     return res.status(200).json(message)
   }
 
   const parseWordsResult = parseWordsArray(wordsArray)
-  // console.log('pwrl', parseWordsResult.length)
   if (parseWordsResult.length === 0) {
     return res.status(200).json(message)
   }
 
   const letter = parseString(parseWordsResult)
-  // console.log('ll', letter.length)
   if (letter.length === 0) {
     return res.status(200).json(message)
   }
 
   return res.status(200).json({
-    message: `the first unique letter in given text is: '${letter}' `
+    message: `the first unique letter in given text is '${letter}'`
   })
 }
 
